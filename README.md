1d6963f69cb51fa1b3df8ed3fd5a7170ec1d5c24
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-02-05 18:09:15 +0900

    3 バリデーション hoge

--- README.md
+++ README.md
@@ -89,13 +89,3 @@ has_one :address
 belongs_to :order
 
 belongs_to :prefecture
-
-
-      t.string :nickname, null: false
-      t.string :email, null: false, unique: true
-      t.string :encrypted_password, null: false
-      t.string :last_name, null: false
-      t.string :first_name, null: false
-      t.string :last_name_kana, null: false
-      t.string :first_name_kana, null: false
-      t.date :birth_date, null: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :orders
+   
+  validates :nickname, presence: true
+  validates :encrypted_password, format: { with: /\A[a-z0-9]+\z/i }           #半角英数字必須
+  #validates :encrypted_password,  #2kai
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }  #kanhirakana?  /\A[ぁ-んァ-ン一-龥]/
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }  #kanhirakana? /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }  #kana? /\A[ァ-ヶー－]+\z/
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }  #kana? /\A[ァ-ヶー－]+\z/
+  validates :birth_date, presence: true
+
+  
 end
+
+
+#VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+#validates :password, format: { with: VALID_PASSWORD_REGEX }
+#validates :password, format: { with: /\A[a-z0-9]+\z/i }
