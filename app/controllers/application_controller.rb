b8ac98521bd6b63a4a83ac16eb6fb586d28e1d5e
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-02-06 15:30:52 +0900

    5 DB保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  #before_action :authenticate_user!
-  #before_action :configure_permitted_parameters, if: :devise_controller?
+#  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   private
 
   def basic_auth
@@ -10,10 +10,16 @@ class ApplicationController < ActionController::Base
     end
   end
 
-#  private
-#  def configure_permitted_parameters
-#    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
-#    )
-#  end
+  #def show
+  #  @nickname = current_user.nickname
+  #end  
+
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
    
-  validates :nickname, presence: true
-  validates :encrypted_password, format: { with: /\A[a-z0-9]+\z/i }           #半角英数字必須
-  #validates :encrypted_password,  #2kai
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }  #kanhirakana?  /\A[ぁ-んァ-ン一-龥]/
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }  #kanhirakana? /\A[ぁ-んァ-ン一-龥]/
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }  #kana? /\A[ァ-ヶー－]+\z/
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }  #kana? /\A[ァ-ヶー－]+\z/
-  validates :birth_date, presence: true
+  # validates :nickname, presence: true
+  # validates :encrypted_password, format: { with: /\A[a-z0-9]+\z/i, message: "半角英数字6文字以上で入力して下さい" }  
+  # #validates :encrypted_password,  #2kai
+  # validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角(漢字・ひらがな・カタカナ)のみで入力して下さい" }  
+  # validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角(漢字・ひらがな・カタカナ)のみで入力して下さい" }  
+  # validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみで入力して下さい" } 
+  # validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみで入力して下さい" } 
+  # validates :birth_date, presence: true
 
   
 end
