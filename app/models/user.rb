ca2375bb1f982185b0007e569ac71020e81cbbd7
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-02-08 15:11:53 +0900

    7 css.バリデ編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  # has_many :items
-  # has_many :orders
+  has_many :items
+  has_many :orders
    
-  # validates :nickname, presence: true
-  # validates :encrypted_password, format: { with: /\A[a-z0-9]+\z/i, message: "半角英数字6文字以上で入力して下さい" }  
-  # #validates :encrypted_password,  #2kai
-  # validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角(漢字・ひらがな・カタカナ)のみで入力して下さい" }  
-  # validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角(漢字・ひらがな・カタカナ)のみで入力して下さい" }  
-  # validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみで入力して下さい" } 
-  # validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみで入力して下さい" } 
-  # validates :birth_date, presence: true
+validates :encrypted_password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/, message: "半角英数字6文字以上で入力して下さい" }
 
-  
+
+with_options presence: true do
+  validates :nickname  
+  validates :birth_date 
 end
 
+with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角(漢字・ひらがな・カタカナ)のみで入力して下さい" } do
+  validates :last_name
+  validates :first_name
+end
 
-#VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-#validates :password, format: { with: VALID_PASSWORD_REGEX }
-#validates :password, format: { with: /\A[a-z0-9]+\z/i }
+with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナのみで入力して下さい" } do
+ validates :last_name_kana
+ validates :first_name_kana
+end 
+
+end 
