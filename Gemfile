2d59ba91cd0272a855b029eb49f07654686c99a3
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-02-09 11:01:38 +0900

    1 itemテーブル・ActiveStorageの導入

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ end
 
 gem 'active_hash'
 gem 'devise'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+
+  # belongs_to :user
+  # has_one :order
+  
+  # has_one_attached :image
+
+  # belongs_to :item_category
+  # belongs_to :item_sales_status
+  # belongs_to :item_shipping
+  # belongs_to :item_prefecture
+  # belongs_to :item_scheduled
+
+end
