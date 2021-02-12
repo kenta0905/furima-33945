bcab9e951c6ce95ba6df07825f938c088f1cbc92
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-02-12 12:17:37 +0900

    2 モデル テーブル作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,14 @@
 class Address < ApplicationRecord
+include ActiveModel::Model
+  attr_accessor :hoge,:fuga...
+
+  belongs_to :order
+
+  belongs_to :prefecture
+
+  # ここにバリデーションの処理を書く
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ApplicationRecord
+include ActiveModel::Model
+  attr_accessor :hoge,:fuga...
+
+  belongs_to :user
+  belongs_to :item
+  has_one :address
+
+  # ここにバリデーションの処理を書く
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+  end
 end
