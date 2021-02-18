8936f1ed7582c1d67aff8626e4830ecf2d6a7f27
kenta0905 <77146336+kenta0905@users.noreply.github.com>
2021-02-18 12:31:20 +0900

    7 自己修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if current_user.id == @item.user_id
+      @item.destroy
      redirect_to root_path
     else
      render :show  
@@ -49,7 +50,7 @@ class ItemsController < ApplicationController
 
 
   def move_to_index
-    if current_user.id == @item.user_id || @item.order.present?
+    if current_user.id != @item.user_id || @item.order.present?
      redirect_to root_path
     end 
   end 
